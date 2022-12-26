e76b1778d84d6beb86fd7c16ec0ff8f9c66a4b15
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-26 17:00:12 +0900

    add error handling when no arguments

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func hasNoArgs(args []string) error {
+	if len(os.Args) == 1 {
+		return fmt.Errorf("error: %s", "Specify linux command to a first argument")
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello!")
+	args := os.Args
+	if err := hasNoArgs(args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	switch args[1] {
+	case "ls":
+		fmt.Println("test")
+	default:
+		fmt.Println("error: Command is not implemented.")
+		os.Exit(1)
+	}
 }
