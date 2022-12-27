4b61622845fd8d335bbc81eb1837c36f70083a6b
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-12-27 13:46:47 +0900

    refactor ls cat

--- cat.go
+++ cat.go
@@ -6,18 +6,21 @@ import (
 )
 
 func cat() {
-	if len(os.Args) == 2 {
-		fmt.Println(fmt.Errorf("golang-linux-command: %s", "argument is required!"))
-		os.Exit(1)
-	} else if len(os.Args) == 3 {
-		file, err := os.ReadFile(os.Args[2])
-		if err != nil {
-			fmt.Println(fmt.Errorf("golang-linux-command: %w", err))
-			os.Exit(1)
-		}
-		fmt.Println(string(file))
-	} else {
+	if len(os.Args) > 3 {
 		fmt.Println(fmt.Errorf("golang-linux-command: %s", "expected arguments are too much"))
 		os.Exit(1)
 	}
+
+	// When no argument
+	if len(os.Args) == 2 {
+		fmt.Println(fmt.Errorf("golang-linux-command: %s", "argument is required!"))
+		os.Exit(1)
+	}
+
+	file, err := os.ReadFile(os.Args[2])
+	if err != nil {
+		fmt.Println(fmt.Errorf("golang-linux-command: %w", err))
+		os.Exit(1)
+	}
+	fmt.Println(string(file))
 }
--- ls.go
+++ ls.go
@@ -7,22 +7,24 @@ import (
 )
 
 func ls() {
+	if len(os.Args) > 3 {
+		fmt.Println(fmt.Errorf("golang-linux-command: %s", "expected arguments are too much"))
+		os.Exit(1)
+	}
+
 	var path string
+	// When path argument is given
 	if len(os.Args) == 3 {
-		// When path argument is given
 		path = os.Args[2]
-	} else if len(os.Args) == 2 {
-		// When no argument
+	}
+	// When no argument
+	if len(os.Args) == 2 {
 		currentDir, err := os.Getwd()
 		if err != nil {
 			fmt.Println(fmt.Errorf("golang-linux-command: %w", err))
 			os.Exit(1)
 		}
 		path = currentDir
-	} else {
-		// usage that is not considered
-		fmt.Println(fmt.Errorf("golang-linux-command: %s", "expected arguments are too much"))
-		os.Exit(1)
 	}
 
 	files, err := os.ReadDir(path)
